4301cd59a37f12d25f242d784970b98e4b5ee71d
Swapnil Konar <swapnilkonar@Swapnils-MacBook-Air.local>
2026-05-26 13:56:23 +0530

    Udpated code

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,10 +16,6 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-subprojects {
     afterEvaluate {
         project.extensions.findByType<com.android.build.gradle.BaseExtension>()?.let { android ->
             android.compileOptions {
@@ -27,7 +23,16 @@ subprojects {
                 targetCompatibility = JavaVersion.VERSION_17
             }
         }
+        project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
     }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
